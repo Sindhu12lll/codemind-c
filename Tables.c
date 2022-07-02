be2eba44f9080034e199d0dea4061f08ac01920c
Sindhu12lll <96376069+Sindhu12lll@users.noreply.github.com>
2022-07-02 21:24:07 +0530

    #include<stdio.h> int main() {     int n,r,i,pro;     scanf("%d%d",&n,&r);     for(i=1;i<=r;i++)     {         if(i%2!=0)         {             pro=n*i;             printf("%d x %d = %d ",n,i,pro);         }     } }

---
+++ Tables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,i,pro;
+    scanf("%d%d",&n,&r);
+    for(i=1;i<=r;i++)
+    {
+        if(i%2!=0)
+        {
+            pro=n*i;
+            printf("%d x %d = %d
+",n,i,pro);
+        }
+    }
+}
