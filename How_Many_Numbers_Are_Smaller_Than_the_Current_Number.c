59a7db117a8a39d60c32ef8a4fb659da2e3dc3a5
Sindhu12lll <96376069+Sindhu12lll@users.noreply.github.com>
2023-07-20 19:23:07 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c=0,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(arr[j]<arr[i])             {                 c++;             }         }         printf("%d ",c);     } }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,c=0,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                c++;
+            }
+        }
+        printf("%d ",c);
+    }
+}
